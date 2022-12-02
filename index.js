f12f90c56c56eb5233d5554d26eedac491aacecf
NIKITA VELIX <nikita.kuznetsov2017@yandex.ru>
2022-12-03 02:28:47 +0300

    wave 2

--- index.js
+++ index.js
@@ -1,34 +1,61 @@
 const ethers = require('ethers');
 
 const PUNK_CONTRACT = "0x4DF0AC645703d26760F1715c2867FA6132dE2816";
-const STAKING_CONTRACT = '0xD84cBbaf27cEB12d4c7fF3bfEC365B4BC798b856';
+const FIRST_STAKING_CONTRACT = '0xD84cBbaf27cEB12d4c7fF3bfEC365B4BC798b856';
+const STAKING_CONTRACT = '0xC98C5d5483faa42c0906958C0A9dB0aA1bed465a';
 const ZENITH_PROVIDER = "https://dataserver-1.zenithchain.co/";
 
 const STAKING_ABI = [
-    'constructor(address,uint8)',
-    'event OwnershipTransferred(address indexed,address indexed)',
+    // ? Events
+    'event RewardStateChanged(uint8,uint8,uint256)',
+    'event Stake(address indexed,uint256,uint256)',
+
+    // ? Staking Status Toggle
+    'function IsStakingEnabled() view returns (bool)',
     'function ToggleStaking(bool)',
-    'function availableRewards(address) view returns (uint256)',
-    'function claimRewards()',
-    'function getRewardsStatus() view returns (uint8)',
-    'function getStakedTokens(address) view returns (tuple(address,uint256)[])',
-    'function getStakingState() view returns (bool)',
-    'function nft() view returns (address)',
-    'function owner() view returns (address)',
-    'function renounceOwnership()',
-    'function rewardsPerHourByStatus(uint8) view returns (uint256)',
-    'function rewardsStatus() view returns (uint8)',
-    'function setRewardStatus(uint8)',
-    'function stake(uint256)',
-    'function stakeMany(uint256[])',
+
+    // ? GOLD
+    'function addToGold(address)',
+    'function goldAddresses(address) view returns (bool)',
+    'function isAddressGold(address) view returns (bool)',
+    'function SetAddressGold(address, bool)',
+    'function SetAddressesGold(address[], bool)',
+    
+    // ? Get Staking info of address
     'function stakerAddress(uint256) view returns (address)',
     'function stakers(address) view returns (uint256, uint256, uint256)',
+    
+    // ? STAKE
+    'function stakePunk(uint256)',
+    'function stakeManyPunks(uint256[])',
+
+    // ? UNSTAKE
+    'function unstakePunk(uint256)',
+    'function unstakeManyPunks(uint256[])',
+
+    // ? CLAIM REWARDS
+    'function claimRewards()',
+    'function availableRewards(address) view returns (uint256)',
+    'function initRewardBalance() view returns (uint256)',
+    'function totalDistributed() view returns (uint256)',
+
+    // ? Reward State
+    'function getRewardState() view returns (uint8)',
+    'function setRewardStatus(uint8)',
+    'function rewardState() view returns (uint8)',
+    'function rewardsPerHourByState(uint8) view returns (uint256)',
+
+    // ? staking information
     'function totalStaked() view returns (uint256)',
-    'function transferOwnership(address)',
-    'function unstake(uint256)',
-    'function unstakeMany(uint256[])',
-    'function withdrawRemains()'
-  ];
+    'function getStakedTokens(address) view returns (tuple(address,uint256)[])',
+
+    // ? contract addresses
+    'function nft() view returns (address)',
+    'function owner() view returns (address)',
+
+    // ? withdraw
+    'function withdrawRemains()',
+];
 
 const NETWORK_SETTINGS = {
     name: 'Zenith Mainent',
